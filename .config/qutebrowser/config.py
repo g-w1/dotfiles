af861d4198e94a990468f0b0650893cbdc6df1ef
jacob <jacoblevgw@gmail.com>
2020-05-14 08:15:36 -0400

    initial commit

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,7 @@
+c.tabs.position = "top"
+c.completion.shrink = True
+c.url.searchengines = {
+"DEFAULT": "https://duckduckgo.com/?q={}",
+"aw": "https://wiki.archlinux.org/?search={}",
+"red": "https://reddit.com/r/{}"
+        }
